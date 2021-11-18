5a63429875b170272bbc479ef9405562da45e4e7
vincemann <v.conrad98@web.de>
2021-11-18 16:16:50 +0100

    adding more logging

--- lib/replace_or_add_line.sh
+++ lib/replace_or_add_line.sh
@@ -7,8 +7,10 @@ line="$3"
 found=$(sudo cat "$file" | grep --count "$pattern")
 
 if [[ "$found" -eq 1 ]];then
+	echo "replacing line containing: $pattern with: $line"
 	sed -i "s/.*$pattern.*/$line/g" "$file"
 elif [[ "$found" -eq 0 ]];then
+	echo "adding line: $line"
 	echo "$pattern" | sudo tee -a "$file"
 else
 	echo "pattern found multiple times, skip replacement"
